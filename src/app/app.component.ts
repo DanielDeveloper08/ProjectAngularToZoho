c666aa2c0185f3401c825f7d76537f1be296f1b0
DanielDeveloper08 <danielprogramando2000@gmail.com>
2023-01-18 17:10:38 -0500

    Cambios generales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,53 @@ export class AppComponent {
         .getCustomers()
         .subscribe( res => {
           this.customers = res.data;
+          console.log(this.customers)
         });
+    }
+
+    view: any[] = [700, 400];
+  
+    // options
+    gradient: boolean = true;
+    showLegend: boolean = true;
+    showLabels: boolean = true;
+    isDoughnut: boolean = false;
+    legendPosition: string = 'below';
+  
+    colorScheme = {
+      domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    };
+  
+    single = [
+      {
+        "name": "Germany",
+        "value": 8940000
+      },
+      {
+        "name": "USA",
+        "value": 5000000
+      },
+      {
+        "name": "France",
+        "value": 7200000
+      },
+        {
+        "name": "UK",
+        "value": 6200000
+      }
+    ];
+  
+  
+    onSelect(data:any): void {
+      console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+    }
+  
+    onActivate(data:any): void {
+      console.log('Activate', JSON.parse(JSON.stringify(data)));
+    }
+  
+    onDeactivate(data:any): void {
+      console.log('Deactivate', JSON.parse(JSON.stringify(data)));
     }
 
 }
